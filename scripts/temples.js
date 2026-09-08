19274ab3f26b7d3a74b00068182a96fbf4bb051f
tuipulotuvaka <tuipulotuv08@gmail.com>
2026-09-09 09:58:06 +1000

    Fix W02 Temple Album responsive menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,22 +2,25 @@ const menuButton = document.querySelector("#menu-button");
 const navigation = document.querySelector("#navigation");
 
 menuButton.addEventListener("click", () => {
-    const isOpen = navigation.classList.toggle("show");
+const isOpen = navigation.classList.toggle("open");
 
-    menuButton.setAttribute("aria-expanded", isOpen);
 
-    if (isOpen) {
-        menuButton.textContent = "✕";
-        menuButton.setAttribute("aria-label", "Close navigation menu");
-    } else {
-        menuButton.textContent = "☰";
-        menuButton.setAttribute("aria-label", "Open navigation menu");
-    }
+menuButton.setAttribute("aria-expanded", isOpen);
+
+if (isOpen) {
+    menuButton.textContent = "✕";
+    menuButton.setAttribute("aria-label", "Close navigation menu");
+} else {
+    menuButton.textContent = "☰";
+    menuButton.setAttribute("aria-label", "Open navigation menu");
+}
+
 });
 
 // Display the current year
-document.querySelector("#currentyear").textContent = new Date().getFullYear();
+document.querySelector("#currentyear").textContent =
+new Date().getFullYear();
 
 // Display the last modified date
 document.querySelector("#lastModified").textContent =
-    `Last Modified: ${document.lastModified}`;
+`Last Modified: ${document.lastModified}`;
